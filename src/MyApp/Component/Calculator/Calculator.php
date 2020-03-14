5960187d6bfd9484ab92454d1f559f489693ef45
alejandromartincruz <socorreitor@gmail.com>
2020-03-14 23:00:49 +0100

    entrega 1 - frameworks

--- src/MyApp/Component/Calculator/Calculator.php
+++ src/MyApp/Component/Calculator/Calculator.php
@@ -10,4 +10,19 @@ class Calculator
         return $v1 + $v2;
     }
 
+    public function subtract(int $v1, int $v2): int
+    {
+        return $v1 - $v2;
+    }
+
+    public function multiply(int $v1, int $v2): int
+    {
+        return $v1 * $v2;
+    }
+
+    public function divide(int $v1, int $v2): float
+    {
+        return $v1 / max($v2, 1);
+    }
+
 }
